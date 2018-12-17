484ffa4f7112584d006324ed5811de1ef3ec90ea
André <45215556+andrealbers@users.noreply.github.com>
2018-12-17 15:14:42 +0100

    Updated

--- Uebungen/Uebung06_1/EinAusgabe.h
+++ Uebungen/Uebung06_1/EinAusgabe.h
@@ -1,3 +1,8 @@
+#ifndef __EINAUSGABE__
+#define __EINAUSGABE__
+
 int eingabezahl();
 int wahlop();
 void ausgabe(float ergebnis, int wahl_op);
+
+#endif // !__EINAUSGABE__
--- Uebungen/Uebung06_1/Rechnen.h
+++ Uebungen/Uebung06_1/Rechnen.h
@@ -1,2 +1,7 @@
+#ifndef __RECHNEN__
+#define __RECHNEN__
+
 int quad_berechnung(int eingezahl);
 float rezi_berechnung(float eingezahl);
+
+#endif // !__RECHNEN__
